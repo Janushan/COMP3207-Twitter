f72abd6cb214936bdcc9c6cea44d64679937d68c
Janu Shan <janu47@live.com>
2019-11-04 11:47:00 +0000

    installed packages and added pages
    - theme customiser
    - added compose tweet page
    - added .env to hide API keys
    - added .env to gitignore
    - installed:
    --- mui-icons
    --- axios
    --- moments

--- src/App.js
+++ src/App.js
@@ -1,31 +1,54 @@
 import React, {Component} from 'react';
 import Grid from '@material-ui/core/Grid';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { StylesProvider } from '@material-ui/styles';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 
 import Header from "./components/header";
-import Home from "./components/pages/home"
-import Profile from "./components/pages/profile"
+import Home from "./components/pages/home";
+import Profile from "./components/pages/profile";
+import ComposeTweet from "./components/pages/compose-tweet";
 
 import './App.css';
+
+const theme = createMuiTheme ({
+  palette: {
+    primary: {
+      main: "#1da1f2",
+      light: "#E8EEF8",
+      dark: "#0c90e1"
+    },
+    secondary: {
+      main: "#657786",
+      light: "#E8EEF8",
+      dark: "#2653A6"
+    },
+  }
+});
 
 class App extends Component {
   render() {
     return (
-      <Router>
-        <div className="app-root">
-          <Grid
-            container
-            direction="column"
-            alignItems="center"
-          >
-            <Header/>
-            <Switch>
-                  <Route path="/" exact component={Home}/>
-                  <Route path="/profile" component={Profile}/>
-                </Switch>
-          </Grid>
-        </div>
-      </Router>
+      <MuiThemeProvider theme={theme}>
+        <StylesProvider injectFirst>
+          <Router>
+            <div className="app-root">
+              <Grid
+                container
+                direction="column"
+                alignItems="center"
+              >
+                <Header/>
+                <Switch>
+                      <Route path="/" exact component={Home}/>
+                      <Route path="/profile" component={Profile}/>
+                      <Route path="/compose/tweet" component={ComposeTweet}/>
+                    </Switch>
+              </Grid>
+            </div>
+          </Router>
+        </StylesProvider>
+      </MuiThemeProvider>
     );
   }
 }
